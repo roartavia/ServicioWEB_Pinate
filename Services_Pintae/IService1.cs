19ea66397fe2e785456032a2936fc77b7f901b29
roartavia <fofillo01@gmail.com>
2016-01-11 07:45:43 -0600

    All the services

---
+++ Services_Pintae/IService1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Collections;
+
+namespace Services_Pintae
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract]
+        string GetData(int value);
+
+        [OperationContract]
+        CompositeType GetDataUsingDataContract(CompositeType composite);
+
+        [OperationContract]
+        string ConexionPrueba();
+
+        [OperationContract]
+        List<TramiteRealizado> ConsultaTramitesCiudadano(string cedula);
+
+        [OperationContract]
+        string ConsultaTramitesPorCedulaJson(string cedula);
+        // TODO: Add your service operations here
+
+        [OperationContract]
+        string ConsultaTramitePorIdJson(int id);
+
+        [OperationContract]
+        string ConsultaTodosLosTipoDeTramites();
+    }
+
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    [DataContract]
+    public class CompositeType
+    {
+        bool boolValue = true;
+        string stringValue = "Hello ";
+
+        [DataMember]
+        public bool BoolValue
+        {
+            get { return boolValue; }
+            set { boolValue = value; }
+        }
+
+        [DataMember]
+        public string StringValue
+        {
+            get { return stringValue; }
+            set { stringValue = value; }
+        }
+    }
+
+    [DataContract]
+    public class TramiteRealizado
+    {
+        int id_tramite = 0;
+        string nombre_tramite = "";
+      
+        [DataMember]
+        public int Id_tramite
+        {
+            get { return id_tramite; }
+            set { id_tramite = value; }
+        }
+
+        [DataMember]
+        public string Nombre_tramite
+        {
+            get { return nombre_tramite; }
+            set { nombre_tramite = value; }
+        }        
+    }
+
+    [DataContract]
+    public class GetTramitesRealizados
+    {
+        public List<TramiteRealizado> listaTramites = new List<TramiteRealizado>();       
+    }
+
+    [DataContract]
+    public class Tramite
+    {
+        string nombre_tramite = "";
+        string fecha_solicitud = "";
+        string estado = "";
+
+        [DataMember]
+        public string Nombre_tramite
+        {
+            get { return nombre_tramite; }
+            set { nombre_tramite = value; }
+        }
+
+        [DataMember]
+        public string Fecha_solicitud
+        {
+            get { return fecha_solicitud; }
+            set { fecha_solicitud = value; }
+        }
+
+        [DataMember]
+        public string Estado
+        {
+            get { return estado; }
+            set { estado = value; }
+        }
+    }
+
+    [DataContract]
+    public class TipoTramite
+    {
+        string nombre_tramite = "";
+        int id_tipo_tramite = 0;
+        
+        [DataMember]
+        public string Nombre_tramite
+        {
+            get { return nombre_tramite; }
+            set { nombre_tramite = value; }
+        }
+
+        [DataMember]
+        public int Id_tipo_tramite
+        {
+            get { return id_tipo_tramite; }
+            set { id_tipo_tramite = value; }
+        }    
+    }
+}
---
+++ Services_Pintae/Service1.svc.cs
@@ -0,0 +1,256 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using MySql.Data.MySqlClient;
+using System.Collections;
+using Newtonsoft.Json;
+
+namespace Services_Pintae
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public string ConexionPrueba()
+        {
+            MySqlConnection connection;
+            string server = "localhost";
+            string database = "pintae";
+            string uid = "Rodolfo";
+            string password = "1234qwer";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+
+                string query = "insert into instituciones (nombre_institucion) values ('MICITT')";
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                string nombre = "";
+
+                while (dataReader.Read())
+                {
+                    nombre = dataReader["nombre_institucion"].ToString();
+                }
+                dataReader.Close();
+                
+                connection.Close();
+
+                return "done";
+            }
+            catch
+            {
+                return "-1";
+            }
+        }
+
+
+        //sin json
+        //recibe la cedula de un ciudano y consulta todos los trámites hechos por ese ciudadano
+        public List<TramiteRealizado> ConsultaTramitesCiudadano(string cedula)
+        {
+            MySqlConnection connection;
+            string server = "localhost";
+            string database = "pintae";
+            string uid = "Rodolfo";
+            string password = "1234qwer";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+
+            List<TramiteRealizado> listTramites = new List<TramiteRealizado>();
+
+            try
+            {
+                connection.Open();
+
+                string query = "select nombre_tramite,id_tramite_solicitado from tramite_solicitado as tramite inner join tipotramite as tipo on tramite.id_tipo_tramite = tipo.id_tramite where tramite.ciudadano = '" + cedula + "'";
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    TramiteRealizado tramiteTemp = new TramiteRealizado();
+                    tramiteTemp.Id_tramite = int.Parse(dataReader["id_tramite_solicitado"].ToString());
+                    tramiteTemp.Nombre_tramite = dataReader["nombre_tramite"].ToString();
+
+                    listTramites.Add(tramiteTemp);
+                }
+
+                dataReader.Close();
+
+                connection.Close();
+
+                return listTramites;
+            }
+            catch
+            {
+                return listTramites;
+            }
+        }
+
+        public string ConsultaTramitesPorCedulaJson(string cedula)
+        {
+            MySqlConnection connection;
+            string server = "localhost";
+            string database = "pintae";
+            string uid = "Rodolfo";
+            string password = "1234qwer";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+
+            GetTramitesRealizados listTramites = new GetTramitesRealizados(); //lista de tramites realizados, es lo que se serializa si hay resultado.
+
+            try {
+                connection.Open();
+
+                string query = "select nombre_tramite,id_tramite_solicitado from tramite_solicitado as tramite inner join tipotramite as tipo on tramite.id_tipo_tramite = tipo.id_tramite where tramite.ciudadano = '" + cedula + "'";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        TramiteRealizado tramiteTemp = new TramiteRealizado();
+                        tramiteTemp.Id_tramite = int.Parse(dataReader["id_tramite_solicitado"].ToString());
+                        tramiteTemp.Nombre_tramite = dataReader["nombre_tramite"].ToString();
+
+                        listTramites.listaTramites.Add(tramiteTemp);
+                    }
+                    dataReader.Close();
+                    connection.Close();
+                    return JsonConvert.SerializeObject(listTramites.listaTramites);
+                }
+                else
+                {
+                    dataReader.Close();
+                    connection.Close();
+                    return "-2"; //No hay trámites para ese ciudadano.
+                }              
+            }
+            catch { return "-1"; } //Error de conexión.
+        }
+
+        //Trae la info del trámite en string formato json.
+        public string ConsultaTramitePorIdJson(int id_tramite_solicitado)
+        {
+            MySqlConnection connection;
+            string server = "localhost";
+            string database = "pintae";
+            string uid = "Rodolfo";
+            string password = "1234qwer";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+
+            Tramite tramiteConsultado = new Tramite();
+
+            try {
+
+                connection.Open();
+
+                string query = "select nombre_tramite,fecha_solicitud,estado from tramite_solicitado as tramite inner join tipotramite as tipo on tramite.id_tipo_tramite = tipo.id_tramite where tramite.id_tramite_solicitado = '" + id_tramite_solicitado + "'";
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+                if (dataReader.HasRows)
+                {
+                    while (dataReader.Read())
+                    {
+                        tramiteConsultado.Nombre_tramite = dataReader["nombre_tramite"].ToString();
+                        tramiteConsultado.Fecha_solicitud = dataReader["fecha_solicitud"].ToString();
+                        tramiteConsultado.Estado = dataReader["estado"].ToString();
+                    }
+                    dataReader.Close();
+                    connection.Close();
+
+                    return JsonConvert.SerializeObject(tramiteConsultado);
+                }
+                else
+                {
+                    dataReader.Close();
+                    connection.Close();
+                    return "-2"; //No existe ese trámite.
+                }
+            }
+            catch { return "-1"; } //Error de conexión.
+        }
+
+        public string ConsultaTodosLosTipoDeTramites()
+        {
+            MySqlConnection connection;
+            string server = "localhost";
+            string database = "pintae";
+            string uid = "Rodolfo";
+            string password = "1234qwer";
+            string connectionString;
+            connectionString = "SERVER=" + server + ";" + "DATABASE=" + database + ";" + "UID=" + uid + ";" + "PASSWORD=" + password + ";";
+            connection = new MySqlConnection(connectionString);
+
+            Tramite tramiteConsultado = new Tramite();
+
+            try
+            {
+                connection.Open();
+
+                string query = "select nombre_tramite,fecha_solicitud,estado from tramite_solicitado as tramite inner join tipotramite as tipo on tramite.id_tipo_tramite = tipo.id_tramite where tramite.id_tramite_solicitado = '" + '1' + "'";
+                //Create Command
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                while (dataReader.Read())
+                {
+                    if (dataReader.HasRows)
+                    {
+                        tramiteConsultado.Nombre_tramite = dataReader["nombre_tramite"].ToString();
+                        tramiteConsultado.Fecha_solicitud = dataReader["fecha_solicitud"].ToString();
+                        tramiteConsultado.Estado = dataReader["estado"].ToString();
+                    }
+                    else
+                    {
+                        dataReader.Close();
+                        connection.Close();
+                        return "No existe ese trámite";
+                    }
+                }
+                dataReader.Close();
+                connection.Close();
+
+                return JsonConvert.SerializeObject(tramiteConsultado);
+            }
+            catch { return "Error"; }
+        }
+
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+    }
+}
